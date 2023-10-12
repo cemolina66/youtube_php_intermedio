f8b13424d318172036dc56bdf88901eb9ad7af77
danielsalina <danielorlandosalinas935@gmail.com>
2023-10-12 17:31:26 -0300

    Redirecciones

---
+++ 04- Redirecciones.php
@@ -0,0 +1,40 @@
+<?php
+
+// header("HTTP/1.1 302 Found");
+// header("Location: 04-nueva_pagina.php");
+// exit;
+
+// header("Refresh: 5; URL=04-nueva_pagina.php");
+?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>Esta página se redirigirá en 5 segundos</p>
+</body>
+
+</html> -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="5;url=04-nueva_pagina.php">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates numquam reprehenderit officia illo quae, corrupti perspiciatis animi quos aliquam voluptate cupiditate architecto aperiam omnis maxime repellat dignissimos porro sunt perferendis!</p>
+</body>
+
+</html>
---
+++ 04-nueva_pagina.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Hola desde la redirección!!!";
